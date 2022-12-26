66188a6b5230c9f1b83ea551b86e4c075d7da577
davidcoats <david@getsafetycone.com>
2022-12-26 14:09:38 -0800

    Initial commit.

---
+++ source/R5T.T0156.Construction/Code/Instances.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+namespace R5T.T0156.Construction
+{
+    public static class Instances
+    {
+		
+    }
+}
---
+++ source/R5T.T0156/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0156
+{
+	/// <summary>
+	/// Documentation marker attributes and interfaces.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
